5339dad652e1aa3921c9c426023bf5d3a0b09462
Bhanu Master <bhanuprasanna527@gmail.com>
2025-03-08 16:45:17 +0530

    stripe error fixed

--- vite-project/src/Checkout/Checkout.jsx
+++ vite-project/src/Checkout/Checkout.jsx
@@ -34,7 +34,7 @@ const Checkout = () => {
       }
 
       try {
-        const response = await axios.post("http://localhost:5000/api/payment/create-payment-intent", {
+        const response = await axios.post("https://project-3-eht1.onrender.com/api/payment/create-payment-intent", {
           amount: totalPrice * 100,
           customerName,
           customerEmail,
